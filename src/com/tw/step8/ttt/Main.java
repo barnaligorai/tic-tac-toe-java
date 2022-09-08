7a17131959d58dbaa1ce2b4d50fbc0bd18177665
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-09-08 15:28:31 +0530

    Added winning logic Reading input from command line

--- src/com/tw/step8/ttt/Main.java
+++ src/com/tw/step8/ttt/Main.java
@@ -4,17 +4,28 @@ import com.tw.step8.ttt.model.Game;
 import com.tw.step8.ttt.model.Player;
 import com.tw.step8.ttt.symbol.Symbol;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     Player player1 = new Player("Ramesh", Symbol.X);
     Player player2 = new Player("Suresh", Symbol.O);
-
     Player[] players = new Player[]{player1, player2};
 
     Game game = new Game(players);
-    System.out.println(game);
+
+    System.out.println(game.displayCells());
+    Scanner scanner = new Scanner(System.in);
+
+    while(!game.isGameOver()) {
+      System.out.println("Enter position");
+      int cellPos = Integer.parseInt(scanner.next());
+      game.play(cellPos);
+      System.out.println(game.displayCells());
+    }
+
+    scanner.close();
+    System.out.println("game over");
   }
+
 }
--- src/com/tw/step8/ttt/model/Game.java
+++ src/com/tw/step8/ttt/model/Game.java
@@ -1,10 +1,14 @@
 package com.tw.step8.ttt.model;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Game {
-  private Player[] players;
+  private static final int[][] winningCombination = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+  private final Player[] players;
+  private final String[] cells = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
   private int currentPlayerIndex;
+  private boolean isGameOver = false;
 
   public Game(Player[] players) {
     this.players = players;
@@ -19,6 +23,53 @@ public class Game {
     this.currentPlayerIndex = (this.currentPlayerIndex + 1) % 2;
   }
 
+  private void markCell(int cellPosition) {
+    int cellIndex = cellPosition - 1;
+    this.cells[cellIndex] = String.valueOf(this.currentPlayer().getSymbol());
+  }
+
+  public void play(int cellPosition) {
+    this.markCell(cellPosition);
+
+    this.isOver();
+    this.changePlayer();
+  }
+
+  private boolean isEvery (int[] winningCombo, String expectedSymbol) {
+    for (int index = 0; index < winningCombo.length; index++) {
+
+      String actualSymbol = this.cells[winningCombo[index]];
+      if (!Objects.equals(actualSymbol, expectedSymbol)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  private boolean isWon() {
+    String symbol = String.valueOf(this.currentPlayer().getSymbol());
+
+    for (int[] combination : winningCombination) {
+      if (isEvery(combination,symbol)){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private void isOver(){
+    if (this.isWon()) {
+      this.isGameOver = true;
+    }
+  }
+
+  public boolean isGameOver() {
+    return isGameOver;
+  }
+  public String displayCells() {
+    return String.format("\n--------\n %s %s %s\n %s %s %s\n %s %s %s\n--------\n",this.cells);
+  }
+
   @Override
   public String toString() {
     return "Game{" +
@@ -26,4 +77,5 @@ public class Game {
             ", currentPlayer=" + this.currentPlayer() +
             '}';
   }
+
 }
