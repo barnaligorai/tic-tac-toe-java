ebec581b77f1b62203c4c8ba2155762b157cac63
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-09-09 13:38:45 +0530

    Changed game drawn logic.

--- src/com/tw/step8/ttt/model/Game.java
+++ src/com/tw/step8/ttt/model/Game.java
@@ -1,10 +1,12 @@
 package com.tw.step8.ttt.model;
 
+import com.tw.step8.ttt.exception.CellNotVacantException;
+
 import java.util.Arrays;
 import java.util.Objects;
 
 public class Game {
-  private static final int[][] winningCombination = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+  private static final int[][] winningCombination = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
   private final Player[] players;
   private final String[] cells = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
   private int currentPlayerIndex;
@@ -18,7 +20,7 @@ public class Game {
     this.currentPlayerIndex = 0;
   }
 
-  public Player currentPlayer(){
+  public Player currentPlayer() {
     return this.players[currentPlayerIndex];
   }
 
@@ -26,71 +28,82 @@ public class Game {
     this.currentPlayerIndex = (this.currentPlayerIndex + 1) % 2;
   }
 
-  private void markCell(int cellPosition) {
-    int cellIndex = cellPosition - 1;
+  private void markCell(int cellIndex) {
     this.cells[cellIndex] = String.valueOf(this.currentPlayer().getSymbol());
     this.movesPlayed += 1;
   }
 
-  public void play(int cellPosition) {
+  private boolean isPositionOccupied(int cellPos) throws CellNotVacantException {
+    return this.cells[cellPos].matches("[XO]");
+  }
+
+  public void play(int cellPosition) throws CellNotVacantException {
+    if (isPositionOccupied(cellPosition)) {
+      throw new CellNotVacantException(cellPosition);
+    }
+
     this.markCell(cellPosition);
 
     if (this.isOver()) {
       return;
     }
+
     this.changePlayer();
   }
 
-  private boolean isEvery (int[] winningComboIndices, String expectedSymbol) {
-
+  private boolean isEvery(int[] winningComboIndices, String expectedSymbol) {
     for (int index : winningComboIndices) {
-
       String actualSymbol = this.cells[index];
       if (!Objects.equals(actualSymbol, expectedSymbol)) {
         return false;
       }
     }
-
     return true;
   }
-  private boolean isWon() {
+
+  private boolean hasPlayerWon() {
     String symbol = String.valueOf(this.currentPlayer().getSymbol());
 
     for (int[] combination : winningCombination) {
-      if (isEvery(combination,symbol)){
+      if (isEvery(combination, symbol)) {
         this.won = this.currentPlayer();
+        this.isGameOver = true;
         return true;
       }
     }
     return false;
   }
 
-  private boolean isOver(){
-    if (this.isWon() || this.isDrawn()) {
-      this.isGameOver = true;
-      return true;
-    }
-      return false;
+  private boolean isOver() {
+    return this.hasPlayerWon() || this.isDrawn();
   }
 
   private boolean isDrawn() {
-    return this.movesPlayed >= 9 && !this.isWon();
+    if(this.movesPlayed >= 9 && !this.hasPlayerWon()){
+     this.isGameDrawn = true;
+      this.isGameOver = true;
+      return true;
+    };
+    return false;
   }
 
   public boolean isGameOver() {
     return isGameOver;
   }
 
-  public String results () {
-    if (this.isGameOver && this.isDrawn()) {
-      this.isGameDrawn = true;
+  public String results() {
+    if (this.isGameDrawn()) {
       return "Game ended in a Draw";
     }
-   return String.format("%s wins", this.won.getName());
+    return String.format("%s wins", this.won.getName());
   }
 
   public String[] getCells() {
     return cells;
+  }
+
+  public boolean isGameDrawn() {
+    return isGameDrawn;
   }
 
   @Override
@@ -101,7 +114,4 @@ public class Game {
             '}';
   }
 
-  public boolean isGameDrawn() {
-    return isGameDrawn;
-  }
 }
--- test/com/tw/step8/ttt/model/GameTest.java
+++ test/com/tw/step8/ttt/model/GameTest.java
@@ -1,5 +1,6 @@
 package com.tw.step8.ttt.model;
 
+import com.tw.step8.ttt.exception.CellNotVacantException;
 import com.tw.step8.ttt.symbol.Symbol;
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ class GameTest {
   @Test
   void currentPlayerShouldReturnTheCurrentPlayer() {
     Player bani = new Player("bani", Symbol.X);
-    Player barnali = new Player("barnali", Symbol.X);
+    Player barnali = new Player("barnali", Symbol.O);
 
     Game game1 = new Game(new Player[]{bani, barnali});
     assertEquals(bani, game1.currentPlayer());
@@ -22,24 +23,47 @@ class GameTest {
   @Test
   void isGameOverShouldReturnFalseForARunningGame() {
     Player bani = new Player("bani", Symbol.X);
-    Player barnali = new Player("barnali", Symbol.X);
+    Player barnali = new Player("barnali", Symbol.O);
 
     Game game = new Game(new Player[]{bani, barnali});
 
     assertFalse(game.isGameOver());
   }
   @Test
-  void isGameOverShouldReturnTrueWhenGameIsOver() {
+  void isGameOverShouldReturnTrueWhenGameIsOver() throws CellNotVacantException {
     Player bani = new Player("bani", Symbol.X);
-    Player barnali = new Player("barnali", Symbol.X);
+    Player barnali = new Player("barnali", Symbol.O);
 
     Game game = new Game(new Player[]{bani, barnali});
     game.play(1);
-    game.play(4);
-    game.play(2);
     game.play(5);
+    game.play(4);
     game.play(3);
+    game.play(7);
 
     assertTrue(game.isGameOver());
+  }
+
+  @Test
+  void isGameDrawn() throws CellNotVacantException {
+    Player bani = new Player("bani", Symbol.X);
+    Player barnali = new Player("barnali", Symbol.O);
+
+    Game game = new Game(new Player[]{bani, barnali});
+    game.play(0);
+    game.play(1);
+
+    game.play(2);
+    game.play(4);
+
+    game.play(3);
+    game.play(6);
+
+    game.play(5);
+    game.play(8);
+
+    game.play(7);
+
+    assertTrue(game.isGameDrawn());
   }
 }
